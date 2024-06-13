205c4e65fcdc612aee2e3951351bd312ee364049
Sagar Bhoge <115706182+sagarbhoge2003@users.noreply.github.com>
2024-06-13 08:15:01 +0530

    Add files via upload

---
+++ Problem10.sql
@@ -0,0 +1,6 @@
+--577. Employee Bonus
+--https://leetcode.com/problems/employee-bonus/?envType=study-plan-v2&envId=top-sql-50
+# Write your MySQL query statement below
+select e.name, b.bonus
+from employee e left join bonus b on e.empid = b.empid
+where b.bonus <1000 or bonus is null
