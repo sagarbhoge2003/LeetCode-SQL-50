b3a1b4b760624a851b1e4cb5a607a8b76997a36f
Sagar Bhoge <115706182+sagarbhoge2003@users.noreply.github.com>
2024-06-20 21:42:21 +0530

    Add files via upload

---
+++ Problem16.sql
@@ -0,0 +1,7 @@
+--1251. Average Selling Price
+--https://leetcode.com/problems/average-selling-price/?envType=study-plan-v2&envId=top-sql-50
+SELECT p.product_id, IFNULL(ROUND(SUM(units*price)/SUM(units),2),0) AS average_price
+FROM Prices p LEFT JOIN UnitsSold u
+ON p.product_id = u.product_id AND
+u.purchase_date BETWEEN start_date AND end_date
+group by product_id
